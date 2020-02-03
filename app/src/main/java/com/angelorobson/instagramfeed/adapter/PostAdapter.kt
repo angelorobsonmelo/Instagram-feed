2781a7732a0d1aaa4ada83e8db07f47168827b54
angelomelo <angelorobsonmelo@gmail.com>
2020-02-03 15:58:44 -0300

    Implemented suggestion recycler view

--- app/src/main/java/com/angelorobson/instagramfeed/adapter/PostAdapter.kt
+++ app/src/main/java/com/angelorobson/instagramfeed/adapter/PostAdapter.kt
@@ -4,28 +4,81 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.angelorobson.instagramfeed.MainActivity
 import com.angelorobson.instagramfeed.R
 import com.angelorobson.instagramfeed.model.Post
 import kotlinx.android.synthetic.main.feed_item.view.*
+import kotlinx.android.synthetic.main.suggestions_recycler_view.view.*
+import kotlin.math.sign
 
-class PostAdapter(private val posts: List<Post>) : RecyclerView.Adapter<PostHolder>() {
+class PostAdapter(private val posts: List<Post>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder =
-        PostHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.feed_item,
-                parent,
-                false
-            )
-        )
+    var twoIsDrag = false
+    private val POST_VIEW = 1
+    private val SUGGESTIONS_VIEW = 0
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View
+
+        return when (viewType) {
+            SUGGESTIONS_VIEW -> {
+                view = inflater.inflate(R.layout.suggestions_recycler_view, parent, false)
+                SuggestionViewHolderVertical(view)
+            }
+            else -> {
+                PostHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                        R.layout.feed_item,
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+
+    }
 
     override fun getItemCount(): Int = posts.size
 
-    override fun onBindViewHolder(holder: PostHolder, position: Int) {
-        holder.bind(posts[position])
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+
+        if (holder.itemViewType == SUGGESTIONS_VIEW) {
+            suggestionView(holder as SuggestionViewHolderVertical)
+        }
+
+        if (holder.itemViewType == POST_VIEW) {
+            val holder = holder as PostHolder
+            holder.bind(posts[position])
+        }
+
     }
 
+    override fun getItemViewType(position: Int): Int {
+        if (position == 1) {
+            return SUGGESTIONS_VIEW
+        }
 
+        return POST_VIEW
+
+    }
+
+}
+
+
+private fun suggestionView(holder: SuggestionViewHolderVertical) {
+    val adapter = SuggestionAdapter(MainActivity.getSuggestions())
+    with(holder.recycler) {
+        layoutManager = androidx.recyclerview.widget.LinearLayoutManager(
+            context,
+            androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        this.adapter = adapter
+    }
 }
 
 
@@ -44,4 +97,15 @@ class PostHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         }
     }
 
+}
+
+class SuggestionViewHolderVertical(view: View) : RecyclerView.ViewHolder(view) {
+
+    var recycler: RecyclerView
+
+    init {
+        with(itemView) {
+            recycler = suggestion_recycler_view
+        }
+    }
 }
---
+++ app/src/main/java/com/angelorobson/instagramfeed/adapter/SuggestionAdapter.kt
@@ -0,0 +1,41 @@
+package com.angelorobson.instagramfeed.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.angelorobson.instagramfeed.R
+import com.angelorobson.instagramfeed.model.Suggestion
+import kotlinx.android.synthetic.main.suggestion_item.view.*
+
+class SuggestionAdapter(private val suggestions: List<Suggestion>) :
+    RecyclerView.Adapter<SuggestionHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuggestionHolder =
+        SuggestionHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.suggestion_item, parent, false)
+        )
+
+
+    override fun getItemCount(): Int = suggestions.size
+
+    override fun onBindViewHolder(holder: SuggestionHolder, position: Int) {
+        holder.bind(suggestions[position])
+    }
+
+
+}
+
+class SuggestionHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(suggestion: Suggestion) {
+        with(itemView) {
+            image_user_suggestion.setImageResource(suggestion.imageUser)
+            textview_username_suggestion.text = suggestion.username
+            textview_info_user_suggestion.text = suggestion.userInfo
+            button_suggestion.text = suggestion.textButton
+        }
+    }
+
+}
