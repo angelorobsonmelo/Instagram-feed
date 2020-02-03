bf3442ddf37844ebce2cc889e9e1ac2040c00b43
angelomelo <angelorobsonmelo@gmail.com>
2020-02-03 13:19:13 -0300

    finished suggestions layout

--- app/src/main/java/com/angelorobson/instagramfeed/MainActivity.kt
+++ app/src/main/java/com/angelorobson/instagramfeed/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.angelorobson.instagramfeed.adapter.MainAdapter
 import com.angelorobson.instagramfeed.model.Post
 import com.angelorobson.instagramfeed.model.Story
+import com.angelorobson.instagramfeed.model.Suggestion
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -30,50 +31,65 @@ class MainActivity : AppCompatActivity() {
 
 
     companion object {
-        fun getStories(): MutableList<Story> {
-            return mutableListOf(
-                Story(R.drawable.jon_snow, "Seu Story"),
-                Story(R.drawable.red_queen, "Red Queen"),
-                Story(R.drawable.sansa, "Sansa Stark"),
-                Story(R.drawable.tyrion_lannister, "Tyrion Lannister"),
-                Story(R.drawable.bran_stark, "Bran Start"),
-                Story(R.drawable.arya_stark, "Arya Start"),
-                Story(R.drawable.khal_drogo, "Khal Drogo")
-            )
-        }
+        fun getStories(): MutableList<Story> = mutableListOf(
+            Story(R.drawable.jon_snow, "Seu Story"),
+            Story(R.drawable.red_queen, "Red Queen"),
+            Story(R.drawable.sansa, "Sansa Stark"),
+            Story(R.drawable.tyrion_lannister, "Tyrion Lannister"),
+            Story(R.drawable.bran_stark, "Bran Start"),
+            Story(R.drawable.arya_stark, "Arya Start"),
+            Story(R.drawable.khal_drogo, "Khal Drogo")
+        )
 
-        fun getPosts(): MutableList<Post> {
-            return mutableListOf(
-                Post(
-                    R.drawable.jon_snow,
-                    "John Snow",
-                    R.drawable.post_1,
-                    "Sansa",
-                    "Daenerys and me looking for dragons!",
-                    R.drawable.jon_snow,
-                    "Há 35 minutos"
-                ),
-                Post(
-                    R.drawable.red_queen,
-                    "Red Queen",
-                    R.drawable.post_2,
-                    "Jonh Snow",
-                    "I am the queen!",
-                    R.drawable.jon_snow,
-                    "Há 3 horas"
-                ),
-                Post(
-                    R.drawable.sansa,
-                    "Sansa Stark",
-                    R.drawable.post_3,
-                    "John Snow",
-                    "my winter clothes. In winter times that keep us warm, it is important to take this care not to die of hypothermia",
-                    R.drawable.jon_snow,
-                    "Há 8 horas"
-                )
+        fun getPosts(): MutableList<Post> = mutableListOf(
+            Post(
+                R.drawable.jon_snow,
+                "John Snow",
+                R.drawable.post_1,
+                "Sansa",
+                "Daenerys and me looking for dragons!",
+                R.drawable.jon_snow,
+                "Há 35 minutos"
+            ),
+            Post(
+                R.drawable.red_queen,
+                "Red Queen",
+                R.drawable.post_2,
+                "Jonh Snow",
+                "I am the queen!",
+                R.drawable.jon_snow,
+                "Há 3 horas"
+            ),
+            Post(
+                R.drawable.sansa,
+                "Sansa Stark",
+                R.drawable.post_3,
+                "John Snow",
+                "my winter clothes. In winter times that keep us warm, it is important to take this care not to die of hypothermia",
+                R.drawable.jon_snow,
+                "Há 8 horas"
             )
-        }
+        )
+
+        fun getSuggestions(): MutableList<Suggestion> = mutableListOf(
+            Suggestion(
+                R.drawable.sansa,
+                "Sansa Stark",
+                "Novo no Instagram",
+                "Seguir"
+            ),
+            Suggestion(
+                R.drawable.tyrion_lannister,
+                "Tyrion Lennister",
+                "Seguido por Red Queen + mais 3 pessoas",
+                "Seguir de volta"
+            ),
+            Suggestion(
+                R.drawable.khal_drogo,
+                "Khal Drogo",
+                "Novo no Instagram",
+                "Seguir"
+            )
+        )
     }
-
-
 }
---
+++ app/src/main/java/com/angelorobson/instagramfeed/model/Suggestion.kt
@@ -0,0 +1,8 @@
+package com.angelorobson.instagramfeed.model
+
+data class Suggestion(
+    val imageUser: Int,
+    val username: String,
+    val userInfo: String,
+    val textButton: String
+)
